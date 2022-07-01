f6a39274a34e07bc5535eab90eba16c23cfdfe8d
samariwa <samuelmariwa@gmail.com>
2022-07-01 06:42:31 -0700

    Size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,3 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
@@ -14,10 +13,10 @@ int main(void)
 	float floatType;
 	long long long_2;
 
-	printf("Size of a char: %c byte(s) sizeof(character)\n");
-	printf("Size of a int: %d byte(s) sizeof(integer)\n");
-	printf("Size of a long int: %lu byte(s) sizeof(longType)\n");
-	printf("Size of a long long int: %lu byte(s) sizeof(long_2)\n");
-	printf("Size of a float: %lu byte(s) sizeof(floatType)\n");
+	printf("Size of a char: %zu byte(s)\n",sizeof(character));
+	printf("Size of a int: %d byte(s)\n",sizeof(integer));
+	printf("Size of a long int: %lu byte(s)\n",sizeof(longType));
+	printf("Size of a long long int: %lu byte(s)\n",sizeof(long_2));
+	printf("Size of a float: %lu byte(s)\n",sizeof(floatType));
         return (0);
 }
