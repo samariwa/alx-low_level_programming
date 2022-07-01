50aec1201117d28add9ca4258c0e57c842535200
samariwa <samuelmariwa@gmail.com>
2022-07-01 03:11:21 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
- * Always return 0 (Success)
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {       
 	puts("\"Programming is like building a multilingual puzzle");
