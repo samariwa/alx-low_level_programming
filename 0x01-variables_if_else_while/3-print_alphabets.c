047b6c4cb48d7c4a929e826aae52eba00cc0227b
samariwa <samuelmariwa@gmail.com>
2022-07-02 01:43:46 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,16 +7,16 @@
  */
 int main(void)
 {
-        char c;
+	char c;
 
-        for (c = 'a'; c <= 'z'; c++)
-        {
-                putchar(c);
-        }
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
 	for (c = 'A'; c <= 'Z'; c++)
-        {
-                putchar(c);
-        }
-        putchar('\n');
-        return (0);
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
 }
