b2321119417a2ebb596ee5b6b423b1d0662cf283
samariwa <samuelmariwa@gmail.com>
2022-07-05 09:54:03 -0700

    Function Prototype

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	
+	for(c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return;
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
