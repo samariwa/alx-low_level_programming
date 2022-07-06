00e888e1a0f72c7b8aa8b0c0f910df6ac30480b0
samariwa <samuelmariwa@gmail.com>
2022-07-05 18:50:30 -0700

    10x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints tha alphabet 10 times
+ *
+ * Returns nothing
+ * */
+
+void print_alphabet_x10(void)
+{
+	int x;
+	char c;
+
+	for(x = '0'; x <= '9'; x++)
+	{
+		for(c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,4 +2,4 @@
 
 void print_alphabet(void);
 int _putchar(char c);
-int print_putchar(void);
+void print_alphabet_x10(void);
