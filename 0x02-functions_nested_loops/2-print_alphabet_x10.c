d011780a9dd6eb82b04f9a328300418752ef10ab
samariwa <samuelmariwa@gmail.com>
2022-07-05 18:52:12 -0700

    Betty

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,16 +4,16 @@
  * print_alphabet_x10 - Prints tha alphabet 10 times
  *
  * Returns nothing
- * */
+ **/
 
 void print_alphabet_x10(void)
 {
 	int x;
 	char c;
 
-	for(x = '0'; x <= '9'; x++)
+	for (x = '0'; x <= '9'; x++)
 	{
-		for(c = 'a'; c <= 'z'; c++)
+		for (c = 'a'; c <= 'z'; c++)
 		{
 			_putchar(c);
 		}
