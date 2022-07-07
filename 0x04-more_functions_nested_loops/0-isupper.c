18083313aeb716f3341d4128de44d3b13c3b9de3
samariwa <samuelmariwa@gmail.com>
2022-07-07 01:14:14 -0700

    Isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if input is uppercase
+ * @c : Input that will be checked by function
+ * Returns 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (isupper(c) == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int _isupper(int c);
