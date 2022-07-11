a26570b317b016a7d7d4bc188c0c6fbacf4d60bf
samariwa <samuelmariwa@gmail.com>
2022-07-11 11:46:01 +0000

    98 Battery St.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates value of input value to 98 using the pointer
+ * @n: address of variable whos value will be changed
+ * The initial value of the variable will have the modified value
+ * after function call
+ *
+ * Returns nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
