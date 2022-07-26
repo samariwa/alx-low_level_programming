a2ecb95814315370d7eed436a5d02bade7cb5c32
samariwa <samuelmariwa@gmail.com>
2022-07-26 05:52:20 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,7 +12,11 @@ int **alloc_grid(int width, int height)
 {
 	int i, j;
 	int **temp;
-
+	
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
 	temp = malloc(height * sizeof(int *));
 
 	for (i = 0; i < height; i++)
