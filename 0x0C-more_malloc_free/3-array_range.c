b2778dae794c4acc80a21f31ca69ff1ec9feb2ae
samariwa <samuelmariwa@gmail.com>
2022-07-28 06:44:00 -0700

    array_range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * *array_range - creates an array of integers
+ * @min: minimum value in the array
+ * @max: maximum value in the array
+ * The values of the array should be arranged acsending order
+ *
+ * Return: pointer to the array if succeeds, elso null
+ */
+int *array_range(int min, int max)
+{
+	int *temp;
+	int diff, i = 0;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	diff = max - min;
+	temp = malloc(diff * sizeof(int));
+	if (temp == NULL)
+	{
+		return (NULL);
+	}
+	while (i <= diff)
+	{
+		temp[i] = min;
+		i++;
+		min++;
+	}
+
+	return (temp);
+
+}
