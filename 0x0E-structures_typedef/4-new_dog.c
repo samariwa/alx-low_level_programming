0814e8564ba1342c6c9e0a4c2f19a5dd8a7d027d
samariwa <samuelmariwa@gmail.com>
2022-08-02 06:18:06 -0700

    A door is what a dog is perpetually on the wrong side of

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,78 @@
+#include "dog.h"
+#include <stdlib.h>
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+/**
+ * *new_dog - creates a new dog
+ * @name: pointer to the name of the dog
+ * @age: pointer to the age of the dog
+ * @owner: pointer to the owner of the dog
+ *
+ * Return the pointer of the new location if successful, else NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_dog;
+
+	new_dog = malloc(sizeof(dog_t));
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	new_dog->name = malloc(_strlen(name) + 1);
+	if (new_dog->name == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->name = _strcpy(new_dog->name, name);
+
+	new_dog->age = age;
+
+	new_dog->owner = malloc(_strlen(owner) + 1);
+	if (new_dog->owner == NULL)
+	{
+		free(new_dog);
+		return (NULL);
+	}
+	new_dog->owner = _strcpy(new_dog->owner, owner);
+
+	return (new_dog);
+}
+
+/**
+ * _strlen - determinates the lenght of a string
+ * @s: pointer to string
+ *
+ * Return: the length
+ */
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	;
+	return (a);
+}
+
+/**
+ * _strcpy - copies a pointed string
+ * @dest: pointer to the destine string
+ * @src: pointer to the source string
+ *
+ * Return: the adress of the destiny string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a = 0;
+
+	while (src[a] != '\0')
+	{
+		dest[a] = src[a];
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
