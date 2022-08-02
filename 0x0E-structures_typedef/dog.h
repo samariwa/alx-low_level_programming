c35ab6c84993ae59a60fdd306b84d73da98c7337
samariwa <samuelmariwa@gmail.com>
2022-08-02 01:22:52 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dog object
+ * @name: The name od the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ *
+ * The name takes in a set of characters and referenced to by a pointer
+ * The age attribute takes in an integer
+ * The owner attribute takes in a set of characters referenced to by a pointer
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
