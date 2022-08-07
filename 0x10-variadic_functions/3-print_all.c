d21814ed95dc9a605b868f1861d37152f1cf62c1
samariwa <samuelmariwa@gmail.com>
2022-08-07 05:15:11 +0000

    Function that prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,10 +9,36 @@
  */
 void print_all(const char * const format, ...)
 {
-	int i;
-	while (format)
+	size_t i = 0;
+	int j;
+	const char specifiers[] = "cifs";
+	const char *type[] = {"char","int","float","string"}; 
+	
+	va_list args;
+
+	va_start(args, format);
+	while (i  < strlen(format))
 	{
+		j = 0;
+		while (j < 4)
+		{
+			if (format[i] == specifiers[j])
+			{
+				if (format[i] == 's' && va_arg(args, char *) == NULL)
+				{
+					printf("(nil");
+				}
+				else
+				{
+					printf("%ld\n", i);
+					printf("%d\n", j);
+					printf("Print %s\n", type[j]);
+				}
+			}
+			j++;	
+		}
 		i++;
 	}
-	printf("%d", i);
+	va_end(args);
+	printf("\n");
 }
