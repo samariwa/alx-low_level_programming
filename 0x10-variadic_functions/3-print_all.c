0fb9b22b1234a14b9fdff031eed7eb9d69302cfa
samariwa <samuelmariwa@gmail.com>
2022-08-07 18:23:06 +0000

    0x10. C - Variadic functions: v 0.4 collab

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,9 +1,10 @@
 #include "variadic_functions.h"
-
+/* check for nulls in strings */
+#define NULL_S(S) ((S) == NULL ? ("(nil)") : (S))
 /**
  * print_all - prints anything
  * @format: list of the types of arguments passed
- * if string is null, prints nill
+ * if string is null, prints nil
  *
  * Return: nothing
  */
@@ -11,9 +12,8 @@ void print_all(const char * const format, ...)
 {
 	size_t i = 0;
 	int j;
+	char *s, *t;
 	const char specifiers[] = "cifs";
-	const char *type[] = {"char","int","float","string"}; 
-	
 	va_list args;
 
 	va_start(args, format);
@@ -24,18 +24,29 @@ void print_all(const char * const format, ...)
 		{
 			if (format[i] == specifiers[j])
 			{
-				if (format[i] == 's' && va_arg(args, char *) == NULL)
+				switch (format[i])
 				{
-					printf("(nil");
-				}
-				else
-				{
-					printf("%ld\n", i);
-					printf("%d\n", j);
-					printf("Print %s\n", type[j]);
+				case 'c':
+					printf("%c\n",
+					       (unsigned char) va_arg(args, int));
+					break;
+				case 's':
+					s = va_arg(args, char *);
+					/* NULL_S does every thing */
+					t = NULL_S(s);
+					printf("%s\n", t);
+					break;
+				case 'i':
+					printf("%d\n", va_arg(args, int));
+					break;
+				case 'f':
+					printf("%f\n", va_arg(args, double));
+					break;
+				default:
+					printf("Print %s\n", "Not available");	
 				}
 			}
-			j++;	
+			j++;
 		}
 		i++;
 	}
