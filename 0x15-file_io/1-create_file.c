34b001fb08b3840a9fc32544b3c2b5733964556f
samariwa <samuelmariwa@gmail.com>
2022-08-22 06:42:49 -0700

    Creating file using file descriptor

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ * @filename: the file which should be read
+ * @content: a null terminating string to write to the file
+ * Description: if the content is NULL, an empty file is created
+ *
+ * Return: 1 on success, else -1
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd;
+	size_t count = _strlen(text_content);
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR, 0600);
+
+	if (fd == -1)
+	{
+		write(1, "Opening failed", 15);
+		return (-1);
+	}
+
+	write(fd, text_content, count);
+
+	close(fd);
+
+	return (1);
+}
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,5 +6,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 int _putchar(char c);
+int create_file(const char *filename, char *text_content);
+int _strlen(char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
