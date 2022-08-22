931bf6968bd11d1891b6c5547018f25e01eda757
samariwa <samuelmariwa@gmail.com>
2022-08-22 18:25:33 +0000

    Append to the end of a file using file descriptor

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,5 +11,51 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int fd, result;
+	size_t count = _strlen(text_content);
 
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+
+	fd = open(filename, O_WRONLY | O_APPEND, 0600);
+
+	if (fd == -1)
+	{
+		return (-1);
+	}
+
+	if (text_content == NULL)
+	{
+		return (1);
+	}
+
+	result = write(fd, text_content, count);
+
+	if (result <= 0)
+	{
+		return (-1);
+	}
+
+	close(fd);
+
+	return (1);
+}
+
+/**
+ * _strlen - count the length of a string
+ * @s: the string that we want to know the length of
+ *
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
