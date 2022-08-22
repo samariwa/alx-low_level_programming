d42aa253ea6bd2462e01b036e43799eaaeccd1f8
samariwa <samuelmariwa@gmail.com>
2022-08-22 06:58:14 -0700

    Code structure for appending text using file descriptor

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: The name of the file in which text is appended at the end
+ * @text_content: the text to be appended at the end of the file
+ * Description: File should not be created if it does not exist,
+ * nothing should be added to the file if the text_content is null
+ *
+ * Return 1 on success, else -1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -8,5 +8,6 @@
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int _strlen(char *s);
+int append_text_to_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 #endif
