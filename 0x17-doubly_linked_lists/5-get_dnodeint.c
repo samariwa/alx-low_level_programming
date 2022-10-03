42de8d14c90ffea860e5a70480ea1a930f548e19
samariwa <samuelmariwa@gmail.com>
2022-10-03 01:59:16 +0000

    Free list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+  
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	count = 0;
+        dlistint_t *temp = *head;
+
+        while (count != index)
+        {
+		if (temp == NULL)
+		{
+			return(NULL);
+		}
+                temp = temp->next;
+		count++;
+        }
+
+        return (temp);
+}
