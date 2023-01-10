40cf160242d8b6ec3fb837b945ff95b2ddbcccc0
samariwa <samuelmariwa@gmail.com>
2023-01-10 06:08:41 +0300

    Fixed syntax errors

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,20 +10,11 @@
 
 int largest_number(int a, int b, int c)
 {
-    	int largest;
+	int largest = a;
 
-  	if ((a >= b) && (b >= c))
-  	{
-        	largest = a;
-  	}
-  	else if ((b >= a) && (a >= c))
-  	{
-    		largest = b;
-  	}
-  	else if ((c >= a) && (c >= b))
-  	{
-     		largest = c;
-  	}
-
+	if (largest < b)
+		largest = b;
+	else if (largest < c)
+		largest = c;
 	return (largest);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,7 +4,7 @@
 #include <stdio.h>
 
 void positive_or_negative(int i);
-void 2-largest_number.c(int x, int y, int z);
+int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
